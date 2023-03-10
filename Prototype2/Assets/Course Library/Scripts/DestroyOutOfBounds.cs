9d72ef9248eb9f57d9e97844f46f0dfc624bf669
DrewAzurinP7 <740723@student.dusd.net>
2023-03-10 15:23:09 -0800

    Added vertical movement to Prototype 2
    Added vertical movement to Prototype 2

--- Prototype2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 50;
     private float lowerBound = -20;
+    private float sideBound = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,16 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        else if (transform.position.x > sideBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        else if(transform.position.x < -sideBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
